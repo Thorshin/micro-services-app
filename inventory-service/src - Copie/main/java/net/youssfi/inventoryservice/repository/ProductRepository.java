703733e650ac6217edc28b42f2c3bbe09d81ec50
Yahya <yah.bouf@gmail.com>
2025-11-10 22:28:41 +0100

    ajout des microservices

---
+++ inventory-service/src - Copie/main/java/net/youssfi/inventoryservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package net.example.inventoryservice.repository;
+
+import net.example.inventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product,String> {
+}
